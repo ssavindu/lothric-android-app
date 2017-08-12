f18d965c0b41c65c6904403db21a6dfd1950fb0c
ssavindu <ssavindu@gmail.com>
2017-08-12 12:25:57 +0530

    second commit

--- app/src/main/java/com/example/savindusanjana/lothric/ToDoFrag.java
+++ app/src/main/java/com/example/savindusanjana/lothric/ToDoFrag.java
@@ -1,12 +1,15 @@
 package com.example.savindusanjana.lothric;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -14,27 +17,37 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ToDoFrag extends Fragment {
+public class ToDoFrag extends Fragment implements View.OnClickListener{
     ArrayList arrayList;
     ArrayAdapter arrayAdapter;
 
-
-
+    FloatingActionButton upButton;
+    View view;
     public ToDoFrag() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_to_do, container, false);
-    }
-
-    public void addNew(View view){
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_to_do, container, false);
+        upButton = (FloatingActionButton) view.findViewById(R.id.btnAdd);
+        upButton.setOnClickListener(this);
+        return view;
     }
 
 
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnAdd:
+                Intent intent = new Intent(getActivity(),newItem.class);
+                startActivity(intent);
+                break;
+
+
+        }
+    }
 }
